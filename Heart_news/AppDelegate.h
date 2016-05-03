831fb28d3649f996e1e867d73a476311a3f7b2dc
徐纪光 <xujiguang@xiyou-mobile-club.local>
2016-05-03 18:08:31 +0800

    Initial Commit

---
+++ Heart_news/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Heart_news
+//
+//  Created by 徐纪光 on 16/5/3.
+//  Copyright © 2016年 jiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Heart_news/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Heart_news
+//
+//  Created by 徐纪光 on 16/5/3.
+//  Copyright © 2016年 jiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
